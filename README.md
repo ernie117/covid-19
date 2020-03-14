6064dd27744f33043810468da36cf962189b7b1c
ernie117 <ern3st@hotmail.co.uk>
2020-03-14 15:44:57 +0000

    restructuring project

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Uses the data found here: https://github.com/CSSEGISandData/COVID-19 to produce graphs showing confirmed cases, recovered cases and deaths.
 
-### Example
+### Example Plot
 ![Plot showing China COVID-19 Cases](china.png)
---
+++ webapp/app.py
@@ -0,0 +1,36 @@
+import os
+
+import yaml
+from flask import Flask, send_from_directory
+
+from webapp.functions import etl_functions
+
+app = Flask(__name__)
+
+with open("config\\config.yml", "r") as f:
+    CONFIG = yaml.load(f, Loader=yaml.FullLoader)
+
+
+@app.route("/<country>")
+def home(country: str):
+    country = country.lower()
+
+    if not check_for_existing_file(country):
+        etl_functions.main(country,
+                           CONFIG["directories"]["covid19Directory"],
+                           CONFIG["directories"]["plotImgDirectory"])
+
+    return send_from_directory("static\\images\\", f"{country}.png")
+
+
+def check_for_existing_file(country: str):
+    with os.scandir("static\\images") as iterator:
+        for filename in iterator:
+            if filename.name.split(".")[0] == country:
+                return filename.name
+
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
