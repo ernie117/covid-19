cf90abc57093da2ccb26e95f52c35fc96c2db7d3
Robert Matthew Stephens <matt.stephens@dvla.gov.uk>
2020-04-04 13:43:41 +0100

    more docstrings

--- webapp/data/extraction/countries_service.py
+++ webapp/data/extraction/countries_service.py
@@ -1,3 +1,7 @@
+"""
+Holds the CountriesService class for dealing with the countries mongo
+collection.
+"""
 from typing import List
 
 from webapp.data.loading.mongo_dao import MongoDAO
@@ -5,6 +9,10 @@ from webapp.loggers.loggers import build_logger
 
 
 class CountriesService:
+    """
+    Class that loads the most up-to-date countries from Mongo dates collection
+    and persists to the Mongo countries collection.
+    """
 
     def __init__(self):
         self.logger = build_logger("CountriesService")
@@ -12,6 +20,12 @@ class CountriesService:
         self.mongo_countries_dao = MongoDAO("countries")
 
     def get_latest_countries(self):
+        """
+        Loads the most up-to-date country list from the latest date document
+        in the dates collection and persists it to the countries collection.
+        Then returns that list of countries to serve as the menu in the flask
+        app.
+        """
         self._load_newest_countries()
         countries_document = self.mongo_countries_dao.get_all()
         countries_document = list(countries_document)
@@ -23,9 +37,6 @@ class CountriesService:
         self._load_most_recent_countries(countries)
 
     def _get_most_up_to_date_countries(self) -> List:
-        """
-        todo
-        """
         data_cursor = self.mongo_dates_dao.get_all()
         most_recent_entry = data_cursor.sort("date", -1).limit(1)
 
@@ -38,11 +49,6 @@ class CountriesService:
         return countries
 
     def _load_most_recent_countries(self, countries: List):
-        """
-        todo
-
-        :return:
-        """
         self.mongo_countries_dao.collection.drop()
 
         document = {"countries": countries}
